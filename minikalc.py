765be02427095b2da7c24a57e7eb3003b98d876b
ginux <rytheed@gmail.com>
2025-05-22 11:41:05 +0500

    added comments

--- minikalc.py
+++ minikalc.py
@@ -2,6 +2,7 @@ num1 = int(input('Введите первое число: '))
 
 num2 = int(input('Введите второе число: '))
 
+#Умножаем значение num1 на 2
 num1 *=2
 
 print ("Result: ", num1 + num2)
@@ -10,5 +11,5 @@ print ("Result: ", num1 ** num2)
 print ("Result: ", num1 // num2)
 
 word = "roRo"
-
+# Ввели переменную word и умножили значение str на 4
 print (word * 4)
