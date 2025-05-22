91512e56367b35a800fd1d66856c5c48d5b14e35
ginux <rytheed@gmail.com>
2025-05-22 11:37:07 +0500

    minicalc and variable

---
+++ minikalc.py
@@ -0,0 +1,14 @@
+num1 = int(input('Введите первое число: '))
+
+num2 = int(input('Введите второе число: '))
+
+num1 *=2
+
+print ("Result: ", num1 + num2)
+print ("Result: ", num1 * num2)
+print ("Result: ", num1 ** num2)
+print ("Result: ", num1 // num2)
+
+word = "roRo"
+
+print (word * 4)
---
+++ variable.py
@@ -0,0 +1,22 @@
+number = 5 # тип данных integer
+
+digit = 5.62323 # тип данных float
+
+# Если "" Закрыть переменную то она будет считаться как строка STR
+test = "-6.702934829349"
+
+word = 'Результат:' # тип данных string
+
+boolean = True # bool тип данных
+
+str_num= '4' # str
+
+#print (word, test, str(digit))
+
+print (word + str(number + int(str_num)))
+
+del number
+
+number = 7
+
+print ("Результат:", number)
