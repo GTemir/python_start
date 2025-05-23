0f60c195bc00a6d3b01c381768a1c075c349d36c
ginux <rytheed@gmail.com>
2025-05-23 09:31:24 +0500

    for

---
+++ for.py
@@ -0,0 +1,13 @@
+#for i in range(0, 16, 2):
+#	print (i)
+
+word = str(input("Введите символ: \n"))
+
+for i in word:
+	if i == "/":
+		print ('Опа это слэш тут...')
+	else:
+		print ("Круто!")
+
+
+
